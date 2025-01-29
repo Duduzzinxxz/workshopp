64b4e2f01e4508872dd33f13e3e5efb54e6591a5
Eduardo Benjamin <duduzzinxxz@gmail.com>
2025-01-29 12:34:45 -0300

    Service layer, component registration

--- src/main/java/com/webedu/study/repositories/UserRepository.java
+++ src/main/java/com/webedu/study/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.webedu.study.repositories;
 
 import com.webedu.study.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
