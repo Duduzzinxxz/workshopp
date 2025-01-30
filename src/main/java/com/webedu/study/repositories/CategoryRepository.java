e096bbdeaa7be497b80b61f1e5d00db90402f63c
Eduardo Benjamin <duduzzinxxz@gmail.com>
2025-01-30 11:55:10 -0300

    Category

---
+++ src/main/java/com/webedu/study/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.webedu.study.repositories;
+
+import com.webedu.study.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
