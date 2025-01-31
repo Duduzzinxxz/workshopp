41105d20e78758e6ef204940012f758169ee2b91
Eduardo Benjamin <duduzzinxxz@gmail.com>
2025-01-31 14:39:29 -0300

    Exception handling - delete

--- src/main/java/com/webedu/study/services/UserService.java
+++ src/main/java/com/webedu/study/services/UserService.java
@@ -2,8 +2,11 @@ package com.webedu.study.services;
 
 import com.webedu.study.entities.User;
 import com.webedu.study.repositories.UserRepository;
+import com.webedu.study.services.exceptions.DatabaseException;
 import com.webedu.study.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,12 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User user) {
