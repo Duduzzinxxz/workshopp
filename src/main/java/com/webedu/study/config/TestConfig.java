3b6b6e89d6fde92f164b84c52ffd8978c865474e
Eduardo Benjamin <duduzzinxxz@gmail.com>
2025-02-04 11:11:03 -0300

    using test profile

--- src/main/java/com/webedu/study/config/TestConfig.java
+++ src/main/java/com/webedu/study/config/TestConfig.java
@@ -55,12 +55,12 @@ public class TestConfig implements CommandLineRunner {
 
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
-        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+        User u1 = new User(null, "Eduardo Benjamin", "eduardo@gmail.com", "5521967809720", "150206");
+        User u2 = new User(null, "Maria Eduarda", "mariasiq@gmail.com", "5521979787695", "081205");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID,u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+        Order o1 = new Order(null, Instant.parse("2024-06-20T19:53:07Z"), OrderStatus.PAID,u1);
+        Order o2 = new Order(null, Instant.parse("2024-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2024-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
@@ -72,9 +72,10 @@ public class TestConfig implements CommandLineRunner {
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
-        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        Payment pay1 = new Payment(null, Instant.parse("2024-06-20T21:53:07Z"), o1);
         o1.setPayment(pay1);
 
         orderRepository.save(o1);
+
     }
 }
